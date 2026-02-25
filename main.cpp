c8001460e497d832f3d449a8d8bb18e7b713ec6a
jnd <oojnd511@gmail.com>
2026-02-26 03:59:23 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    
+    int x;
+    int y;
+    
+    std::cout << "x: ";
+    std::cin >> x;
+    std::cout << "y: ";
+    std::cin >> y;
+    
+    std::cout << "summation: " << x + y << "\n";
+    std::cout << "subtraction: " << x - y << "\n;
+    std::cout << "multiplication: " << x * y << "\n";
+    std::cout << "division: " << x / y << "\n";
+    
+    return 0;
+}
